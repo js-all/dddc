5cd97a28c452fd23c0e475faa70734e07d326dd1
js-all <titouangoulois29@gmail.com>
2020-10-11 23:12:16 +0200

    math done now projection

--- main.c
+++ main.c
@@ -1,207 +1,110 @@
 #include <stdio.h>
 #include <math.h>
+#include "matrixs.c"
+#include <string.h>
 
-typedef int mat4[4][4];
-typedef int vec3[3];
+#define zNear 0.1
+#define zFar 100
+#define meshLen 8
+#define size 50
 
+vec3 objRot = {0, 0, 0};
+vec3 objSca = {1, 1, 1};
+vec3 objTra = {0, 0, 1};
 
-void m4mmul(mat4 *out, mat4 a, mat4 b)
+float fov;
+float aspect = 16 / 9;
+
+static vec3 mesh[meshLen] = {
+    {-1, -1, -1},
+    {1, -1, -1},
+    {-1, -1, 1},
+    {1, -1, 1},
+    {-1, 1, -1},
+    {1, 1, -1},
+    {-1, 1, 1},
+    {1, 1, 1}};
+
+void computeModelMatrix(mat4 out, vec3 scale, vec3 rotation, vec3 translation)
 {
-    *out[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0] + a[0][2] * b[2][0] + a[0][3] * b[3][0];
-    *out[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1] + a[0][2] * b[2][1] + a[0][3] * b[3][1];
-    *out[0][2] = a[0][0] * b[0][2] + a[0][1] * b[1][2] + a[0][2] * b[2][2] + a[0][3] * b[3][2];
-    *out[0][3] = a[0][0] * b[0][3] + a[0][1] * b[1][3] + a[0][2] * b[2][3] + a[0][3] * b[3][3];
 
-    *out[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0] + a[1][2] * b[2][0] + a[1][3] * b[3][0];
-    *out[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1] + a[1][2] * b[2][1] + a[1][3] * b[3][1];
-    *out[1][2] = a[1][0] * b[0][2] + a[1][1] * b[1][2] + a[1][2] * b[2][2] + a[1][3] * b[3][2];
-    *out[1][3] = a[1][0] * b[0][3] + a[1][1] * b[1][3] + a[1][2] * b[2][3] + a[1][3] * b[3][3];
-
-    *out[2][0] = a[2][0] * b[0][0] + a[2][1] * b[1][0] + a[2][2] * b[2][0] + a[2][3] * b[3][0];
-    *out[2][1] = a[2][0] * b[0][1] + a[2][1] * b[1][1] + a[2][2] * b[2][1] + a[2][3] * b[3][1];
-    *out[2][2] = a[2][0] * b[0][2] + a[2][1] * b[1][2] + a[2][2] * b[2][2] + a[2][3] * b[3][2];
-    *out[2][3] = a[2][0] * b[0][3] + a[2][1] * b[1][3] + a[2][2] * b[2][3] + a[2][3] * b[3][3];
-
-    *out[3][0] = a[3][0] * b[0][0] + a[3][1] * b[1][0] + a[3][2] * b[2][0] + a[3][3] * b[3][0];
-    *out[3][1] = a[3][0] * b[0][1] + a[3][1] * b[1][1] + a[3][2] * b[2][1] + a[3][3] * b[3][1];
-    *out[3][2] = a[3][0] * b[0][2] + a[3][1] * b[1][2] + a[3][2] * b[2][2] + a[3][3] * b[3][2];
-    *out[3][3] = a[3][0] * b[0][3] + a[3][1] * b[1][3] + a[3][2] * b[2][3] + a[3][3] * b[3][3];
+    vec3 axisX = {1, 0, 0};
+    vec3 axisY = {0, 1, 0};
+    vec3 axisZ = {0, 0, 1};
+    m4identity(out);
+    m4scale(out, out, scale[0], scale[1], scale[2]);
+    m4rotate(out, out, rotation[0], axisX);
+    m4rotate(out, out, rotation[1], axisY);
+    m4rotate(out, out, rotation[2], axisZ);
+    m4translate(out, out, translation[0], translation[1], translation[2]);
 }
 
-void m4identity(mat4 *out)
+float rad(float in)
 {
-    *out[0][0] = 1;
-    *out[0][1] = 0;
-    *out[0][2] = 0;
-    *out[0][3] = 0;
-
-    *out[1][0] = 0;
-    *out[1][1] = 1;
-    *out[1][2] = 0;
-    *out[1][3] = 0;
-
-    *out[2][0] = 0;
-    *out[2][1] = 0;
-    *out[2][2] = 1;
-    *out[2][3] = 0;
-
-    *out[3][0] = 0;
-    *out[3][1] = 0;
-    *out[3][2] = 0;
-    *out[3][3] = 1;
+    return in / 180 * M_PI;
 }
 
-void m4translate(mat4 *out, int x, int y, int z)
+int main(int argc, char *argv[])
 {
-    *out[0][3] += x;
-    *out[1][3] += y;
-    *out[2][3] += z;
-}
+    fov = rad(90);
+    objRot[1] = rad(45);
 
-void m4scale(mat4 *out, int sx, int sy, int sz)
-{
-    *out[0][0] *= sx;
-    *out[1][1] *= sy;
-    *out[2][2] *= sz;
-}
 
-void m4rotate(mat4 *out, mat4 a, int rad, vec3 axis)
-{
+    mat4 modelMatrix;
+    computeModelMatrix(modelMatrix, objSca, objRot, objTra);
 
-    int x = axis[0];
-    int y = axis[1];
-    int z = axis[2];
+    mat4 projectionMatrix;
+    m4identity(projectionMatrix);
+    m4perspective(projectionMatrix, fov, zNear, zFar, aspect);
 
-    int len = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    int s, c, t;
-    int a00, a01, a02, a03;
-    int a10, a11, a12, a13;
-    int a20, a21, a22, a23;
-    int b00, b01, b02;
-    int b10, b11, b12;
-    int b20, b21, b22;
+    mat4 masterMatrix;
+    m4mul(masterMatrix, modelMatrix, projectionMatrix);
 
-    len = 1 / len;
-    x *= len;
-    y *= len;
-    z *= len;
-    s = sin(rad);
-    c = cos(rad);
-    t = 1 - c;
+    vec3 projectedPoints[meshLen];
 
-    a00 = a[0][0];
-    a01 = a[0][1];
-    a02 = a[0][2];
-    a03 = a[0][3];
-    a10 = a[1][0];
-    a11 = a[1][1];
-    a12 = a[1][2];
-    a13 = a[1][3];
-    a20 = a[2][0];
-    a21 = a[2][1];
-    a22 = a[2][2];
-    a23 = a[2][3];
-
-    // Construct the elements of the rotation matrix
-    b00 = x * x * t + c;
-    b01 = y * x * t + z * s;
-    b02 = z * x * t - y * s;
-    b10 = x * y * t - z * s;
-    b11 = y * y * t + c;
-    b12 = z * y * t + x * s;
-    b20 = x * z * t + y * s;
-    b21 = y * z * t - x * s;
-    b22 = z * z * t + c;
-
-    // Perform rotation-specific matrix multiplication
-    *out[0][0] = a00 * b00 + a10 * b01 + a20 * b02;
-    *out[0][1] = a01 * b00 + a11 * b01 + a21 * b02;
-    *out[0][2] = a02 * b00 + a12 * b01 + a22 * b02;
-    *out[0][3] = a03 * b00 + a13 * b01 + a23 * b02;
-    *out[1][0] = a00 * b10 + a10 * b11 + a20 * b12;
-    *out[1][1] = a01 * b10 + a11 * b11 + a21 * b12;
-    *out[1][2] = a02 * b10 + a12 * b11 + a22 * b12;
-    *out[1][3] = a03 * b10 + a13 * b11 + a23 * b12;
-    *out[2][0] = a00 * b20 + a10 * b21 + a20 * b22;
-    *out[2][1] = a01 * b20 + a11 * b21 + a21 * b22;
-    *out[2][2] = a02 * b20 + a12 * b21 + a22 * b22;
-    *out[2][3] = a03 * b20 + a13 * b21 + a23 * b22;
-
-    if (a != *out)
+    for (int i = 0; i < meshLen; i++)
     {
-        // If the source and destination differ, copy the unchanged last row
-        *out[3][0] = a[3][0];
-        *out[3][1] = a[3][1];
-        *out[3][2] = a[3][2];
-        *out[3][3] = a[3][3];
-    }
-}
+        vec3 resPoint;
+        v3applyMat4(resPoint, mesh[i], masterMatrix);
+        projectedPoints[i][0] = resPoint[0];
+        projectedPoints[i][1] = resPoint[1];
+        projectedPoints[i][2] = resPoint[2];
+    };
 
-void m4perspective(mat4 *out, mat4 a, mat4 b, int fovy, int near, int far, int aspect)
-{
-    int f = 1.0 / tan(fovy / 2);
-    int nf;
+    char resStr[size * size];
 
-    *out[0][0] = f / aspect;
-    *out[0][1] = 0;
-    *out[0][2] = 0;
-    *out[0][3] = 0;
-    *out[1][0] = 0;
-    *out[1][1] = f;
-    *out[1][2] = 0;
-    *out[1][3] = 0;
-    *out[2][0] = 0;
-    *out[2][1] = 0;
-    *out[2][2] = -1;
-    *out[2][3] = 0;
-    *out[3][0] = 0;
-    *out[3][2] = 0;
-
-    if (far != NULL && far != INFINITY)
+    for (int y = 0; y < size; y++)
     {
-        nf = 1 / (near - far);
-        *out[2][2] = (far + near) * nf;
-        *out[3][0] = 2 * far * near * nf;
+        for (int x = 0; x < size; x++)
+        {
+            for (int j = 0; j < meshLen; j++)
+            {
+                int px = floor((projectedPoints[j][0] + 1.0) / 2.0 * size);
+                int py = floor((projectedPoints[j][1] + 1.0) / 2.0 * size);
+                if (px == x && py == y)
+                {
+                    resStr[y * size + x] = '@';
+                }
+                else
+                {
+                    resStr[y * size + x] = 'o';
+                }
+            }
+        }
     }
-    else
+
+    for (int s = 0; s < strlen(resStr); s++)
     {
-        *out[2][2] = -1;
-        *out[3][0] = -2 * near;
+        printf("%c", resStr[s]);
+        if (s > 0 && s % size == 0)
+        {
+            printf("\n");
+        }
     }
-}
 
-void v3add(vec3 *out, vec3 a, vec3 b) {
-    *out[0] = a[0] + b[0];
-    *out[1] = a[1] + b[1];
-    *out[2] = a[2] + b[2];
-}
+    objRot[0] += 0.01;
+    objRot[1] += 0.02;
+    objRot[2] += 0.005;
 
-void v3sub(vec3 *out, vec3 a, vec3 b) {
-    *out[0] = a[0] - b[0];
-    *out[1] = a[1] - b[1];
-    *out[2] = a[2] - b[2];
-}
-
-void v3mul(vec3 *out, vec3 a, vec3 b) {
-    *out[0] = a[0] * b[0];
-    *out[1] = a[1] * b[1];
-    *out[2] = a[2] * b[2];
-}
-
-void v3div(vec3 *out, vec3 a, vec3 b) {
-    *out[0] = a[0] / b[0];
-    *out[1] = a[1] / b[1];
-    *out[2] = a[2] / b[2];
-}
-
-int v3dot(vec3 a, vec3 b) {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-int v3len(vec3 a) {
-    return sqrt(vec3sqrLen(a));
-}
-
-int vec3sqrLen(vec3 a) {
-    return pow(a[0], 2) + pow(a[1], 2) + pow(a[2], 2);
+    return 0;
 }
---
+++ matrixs.c
@@ -0,0 +1,256 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef float mat4[4][4];
+typedef float vec3[3];
+
+void m4mul(mat4 out, mat4 a, mat4 b)
+{
+    out[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0] + a[0][2] * b[2][0] + a[0][3] * b[3][0];
+    out[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1] + a[0][2] * b[2][1] + a[0][3] * b[3][1];
+    out[0][2] = a[0][0] * b[0][2] + a[0][1] * b[1][2] + a[0][2] * b[2][2] + a[0][3] * b[3][2];
+    out[0][3] = a[0][0] * b[0][3] + a[0][1] * b[1][3] + a[0][2] * b[2][3] + a[0][3] * b[3][3];
+
+    out[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0] + a[1][2] * b[2][0] + a[1][3] * b[3][0];
+    out[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1] + a[1][2] * b[2][1] + a[1][3] * b[3][1];
+    out[1][2] = a[1][0] * b[0][2] + a[1][1] * b[1][2] + a[1][2] * b[2][2] + a[1][3] * b[3][2];
+    out[1][3] = a[1][0] * b[0][3] + a[1][1] * b[1][3] + a[1][2] * b[2][3] + a[1][3] * b[3][3];
+
+    out[2][0] = a[2][0] * b[0][0] + a[2][1] * b[1][0] + a[2][2] * b[2][0] + a[2][3] * b[3][0];
+    out[2][1] = a[2][0] * b[0][1] + a[2][1] * b[1][1] + a[2][2] * b[2][1] + a[2][3] * b[3][1];
+    out[2][2] = a[2][0] * b[0][2] + a[2][1] * b[1][2] + a[2][2] * b[2][2] + a[2][3] * b[3][2];
+    out[2][3] = a[2][0] * b[0][3] + a[2][1] * b[1][3] + a[2][2] * b[2][3] + a[2][3] * b[3][3];
+
+    out[3][0] = a[3][0] * b[0][0] + a[3][1] * b[1][0] + a[3][2] * b[2][0] + a[3][3] * b[3][0];
+    out[3][1] = a[3][0] * b[0][1] + a[3][1] * b[1][1] + a[3][2] * b[2][1] + a[3][3] * b[3][1];
+    out[3][2] = a[3][0] * b[0][2] + a[3][1] * b[1][2] + a[3][2] * b[2][2] + a[3][3] * b[3][2];
+    out[3][3] = a[3][0] * b[0][3] + a[3][1] * b[1][3] + a[3][2] * b[2][3] + a[3][3] * b[3][3];
+}
+
+void m4identity(mat4 out)
+{
+    out[0][0] = 1;
+    out[0][1] = 0;
+    out[0][2] = 0;
+    out[0][3] = 0;
+
+    out[1][0] = 0;
+    out[1][1] = 1;
+    out[1][2] = 0;
+    out[1][3] = 0;
+
+    out[2][0] = 0;
+    out[2][1] = 0;
+    out[2][2] = 1;
+    out[2][3] = 0;
+
+    out[3][0] = 0;
+    out[3][1] = 0;
+    out[3][2] = 0;
+    out[3][3] = 1;
+}
+
+void m4translate(mat4 out, mat4 a, float x, float y, float z)
+{
+    out[0][3] = a[0][3] + x;
+    out[1][3] = a[1][3] + y;
+    out[2][3] = a[2][3] + z;
+}
+
+void m4scale(mat4 out, mat4 a, float sx, float sy, float sz)
+{
+    out[0][3] = a[0][3] * sx;
+    out[1][3] = a[1][3] * sy;
+    out[2][3] = a[2][3] * sz;
+}
+
+void m4rotate(mat4 out, mat4 a, float rad, vec3 axis)
+{
+
+    float x = axis[0];
+    float y = axis[1];
+    float z = axis[2];
+
+    float len = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    float s, c, t;
+    float a00, a01, a02, a03;
+    float a10, a11, a12, a13;
+    float a20, a21, a22, a23;
+    float b00, b01, b02;
+    float b10, b11, b12;
+    float b20, b21, b22;
+
+    len = 1 / len;
+    x *= len;
+    y *= len;
+    z *= len;
+    s = sin(rad);
+    c = cos(rad);
+    t = 1 - c;
+
+    a00 = a[0][0];
+    a01 = a[0][1];
+    a02 = a[0][2];
+    a03 = a[0][3];
+    a10 = a[1][0];
+    a11 = a[1][1];
+    a12 = a[1][2];
+    a13 = a[1][3];
+    a20 = a[2][0];
+    a21 = a[2][1];
+    a22 = a[2][2];
+    a23 = a[2][3];
+
+    // Construct the elements of the rotation matrix
+    b00 = x * x * t + c;
+    b01 = y * x * t + z * s;
+    b02 = z * x * t - y * s;
+    b10 = x * y * t - z * s;
+    b11 = y * y * t + c;
+    b12 = z * y * t + x * s;
+    b20 = x * z * t + y * s;
+    b21 = y * z * t - x * s;
+    b22 = z * z * t + c;
+
+    // Perform rotation-specific matrix multiplication
+    out[0][0] = a00 * b00 + a10 * b01 + a20 * b02;
+    out[0][1] = a01 * b00 + a11 * b01 + a21 * b02;
+    out[0][2] = a02 * b00 + a12 * b01 + a22 * b02;
+    out[0][3] = a03 * b00 + a13 * b01 + a23 * b02;
+    out[1][0] = a00 * b10 + a10 * b11 + a20 * b12;
+    out[1][1] = a01 * b10 + a11 * b11 + a21 * b12;
+    out[1][2] = a02 * b10 + a12 * b11 + a22 * b12;
+    out[1][3] = a03 * b10 + a13 * b11 + a23 * b12;
+    out[2][0] = a00 * b20 + a10 * b21 + a20 * b22;
+    out[2][1] = a01 * b20 + a11 * b21 + a21 * b22;
+    out[2][2] = a02 * b20 + a12 * b21 + a22 * b22;
+    out[2][3] = a03 * b20 + a13 * b21 + a23 * b22;
+
+    if (a != out)
+    {
+        // If the source and destination differ, copy the unchanged last row
+        out[3][0] = a[3][0];
+        out[3][1] = a[3][1];
+        out[3][2] = a[3][2];
+        out[3][3] = a[3][3];
+    }
+}
+
+void m4perspective(mat4 out, float fovy, float near, float far, float aspect)
+{
+    float f = 1.0 / tan(fovy / 2);
+    float nf;
+
+    out[0][0] = f / aspect;
+    out[0][1] = 0;
+    out[0][2] = 0;
+    out[0][3] = 0;
+    out[1][0] = 0;
+    out[1][1] = f;
+    out[1][2] = 0;
+    out[1][3] = 0;
+    out[2][0] = 0;
+    out[2][1] = 0;
+    out[2][2] = -1;
+    out[2][3] = 0;
+    out[3][0] = 0;
+    out[3][2] = 0;
+
+    if (far != INFINITY)
+    {
+        nf = 1 / (near - far);
+        out[2][2] = (far + near) * nf;
+        out[3][0] = 2 * far * near * nf;
+    }
+    else
+    {
+        out[2][2] = -1;
+        out[3][0] = -2 * near;
+    }
+}
+
+void v3add(vec3 out, vec3 a, vec3 b)
+{
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2];
+}
+
+void v3addFloat(vec3 out, vec3 a, float b)
+{
+    out[0] = a[0] + b;
+    out[1] = a[1] + b;
+    out[2] = a[2] + b;
+}
+
+void v3sub(vec3 out, vec3 a, vec3 b)
+{
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
+}
+
+void v3subFloat(vec3 out, vec3 a, float b)
+{
+    out[0] = a[0] - b;
+    out[1] = a[1] - b;
+    out[2] = a[2] - b;
+}
+
+void v3mul(vec3 out, vec3 a, vec3 b)
+{
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
+}
+
+void v3mulFloat(vec3 out, vec3 a, float b)
+{
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
+}
+
+void v3div(vec3 out, vec3 a, vec3 b)
+{
+    out[0] = a[0] / b[0];
+    out[1] = a[1] / b[1];
+    out[2] = a[2] / b[2];
+}
+
+void v3divFloat(vec3 out, vec3 a, float b)
+{
+    out[0] = a[0] / b;
+    out[1] = a[1] / b;
+    out[2] = a[2] / b;
+}
+
+float v3dot(vec3 a, vec3 b)
+{
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+float v3sqrLen(vec3 a)
+{
+    return pow(a[0], 2) + pow(a[1], 2) + pow(a[2], 2);
+}
+
+float v3len(vec3 a)
+{
+    return sqrt(v3sqrLen(a));
+}
+
+void v3normalize(vec3 out, vec3 a)
+{
+    float len = v3len(a);
+    out[0] = a[0] / len;
+    out[1] = a[1] / len;
+    out[2] = a[2] / len;
+}
+
+void v3applyMat4(vec3 out, vec3 a, mat4 b)
+{
+    out[0] = b[0][0] * a[0] + b[0][1] * a[1] + b[0][2] * a[2] + b[0][3];
+    out[1] = b[1][0] * a[0] + b[1][1] * a[1] + b[1][2] * a[2] + b[1][3];
+    out[2] = b[2][0] * a[0] + b[2][1] * a[1] + b[2][2] * a[2] + b[2][3];
+}
